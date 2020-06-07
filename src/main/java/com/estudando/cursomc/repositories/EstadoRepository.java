e7c7d2e30cb55d536ed7b3de8863fa1317ff2754
Lucas Aranha <21315381@anhembimorumbi.edu.br>
2020-06-07 20:40:45 -0300

    Estado e cidade

---
+++ src/main/java/com/estudando/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.estudando.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.estudando.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
