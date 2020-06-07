5f8a598c32edebd987a8b03309b77694ed3502d0
Lucas Aranha <21315381@anhembimorumbi.edu.br>
2020-06-07 19:21:26 -0300

    Ajustes no endpoint /categorias/{id}

---
+++ src/main/java/com/estudando/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.estudando.cursomc.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.estudando.cursomc.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		String error = "Objeto não encontrado";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError erro = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(erro);
+	}
+}
--- src/main/java/com/estudando/cursomc/services/CategoriaService.java
+++ src/main/java/com/estudando/cursomc/services/CategoriaService.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 
 import com.estudando.cursomc.domain.Categoria;
 import com.estudando.cursomc.repositories.CategoriaRepository;
+import com.estudando.cursomc.services.exceptions.ObjectNotFoundException;
+
+
 
 @Service
 public class CategoriaService {
@@ -17,7 +20,7 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Id " + id + " não encontrado, tipo: " + Categoria.class.getName()));
 	}
 	
 }
