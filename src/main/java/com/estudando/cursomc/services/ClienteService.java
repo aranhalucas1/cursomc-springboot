4a1b557c368ec81cac6f60d783e5e01adb3799ad
Lucas Aranha <21315381@anhembimorumbi.edu.br>
2020-06-08 17:49:41 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/estudando/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.estudando.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.estudando.cursomc.domain.Cliente;
+import com.estudando.cursomc.repositories.ClienteRepository;
+import com.estudando.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer cod) {
+		Optional<Cliente> obj = repo.findById(cod);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Cliente cod: " + cod + " não encontrado"));
+	}
+	
+}
