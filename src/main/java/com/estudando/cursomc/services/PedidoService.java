50d6fe59fdba04529586bf459de1bf3c709751b8
Lucas Aranha <21315381@anhembimorumbi.edu.br>
2020-06-09 12:44:32 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/estudando/cursomc/services/PedidoService.java
@@ -0,0 +1,27 @@
+package com.estudando.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.estudando.cursomc.domain.Categoria;
+import com.estudando.cursomc.domain.Pedido;
+import com.estudando.cursomc.repositories.PedidoRepository;
+import com.estudando.cursomc.services.exceptions.ObjectNotFoundException;
+
+
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Id " + id + " não encontrado, tipo: " + Categoria.class.getName()));
+	}
+	
+}
