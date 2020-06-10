dba20b6e84bbe5fcc8239bd587e3cd87e31930d9
Lucas Aranha <21315381@anhembimorumbi.edu.br>
2020-06-10 18:20:56 -0300

    Deletando uma categoria com DELETE

--- src/main/java/com/estudando/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/estudando/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.estudando.cursomc.services.exceptions.DataIntegrityException;
 import com.estudando.cursomc.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -18,6 +19,14 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> resourceNotFound(ObjectNotFoundException e, HttpServletRequest request) {
 		String error = "Objeto não encontrado";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError erro = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(erro);
+	}
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> resourceNotFound(DataIntegrityException e, HttpServletRequest request) {
+		String error = "Objeto não encontrado";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError erro = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(erro);
 	}
--- src/main/java/com/estudando/cursomc/services/CategoriaService.java
+++ src/main/java/com/estudando/cursomc/services/CategoriaService.java
@@ -3,34 +3,43 @@ package com.estudando.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.estudando.cursomc.domain.Categoria;
 import com.estudando.cursomc.repositories.CategoriaRepository;
+import com.estudando.cursomc.services.exceptions.DataIntegrityException;
 import com.estudando.cursomc.services.exceptions.ObjectNotFoundException;
-
-
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
-	
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Id " + id + " não encontrado, tipo: " + Categoria.class.getName()));
+		return obj.orElseThrow(
+				() -> new ObjectNotFoundException("Id " + id + " não encontrado, tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
 	}
-	
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categoria contendo produtos");
+		}
+	}
+
 }
