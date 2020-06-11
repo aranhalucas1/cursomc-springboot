50efff175c828d6f08b1890927e520d41c05bf99
Lucas Aranha <21315381@anhembimorumbi.edu.br>
2020-06-10 23:39:00 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/estudando/cursomc/services/CategoriaService.java
+++ src/main/java/com/estudando/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.estudando.cursomc.domain.Categoria;
@@ -42,9 +45,14 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possivel excluir uma categoria contendo produtos");
 		}
 	}
-	
+
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+
+	public Page<Categoria> findPage(Integer page, Integer size, String direction, String orderBy) {
+		PageRequest pageRequest = PageRequest.of(page, size, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 
 }
