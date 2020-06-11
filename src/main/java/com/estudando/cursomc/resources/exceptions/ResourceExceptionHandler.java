e3b0128fbb906c3a57b5c5763f789ee9bd1706a2
Lucas Aranha <21315381@anhembimorumbi.edu.br>
2020-06-11 06:08:10 -0300

    Personalizando o retorno da validacao

--- src/main/java/com/estudando/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/estudando/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -28,6 +30,17 @@ public class ResourceExceptionHandler {
 		String error = "Objeto não encontrado";
 		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError erro = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(erro);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		String error = "Erro de validação";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		ValidationError erro = new ValidationError(Instant.now(), status.value(), error, "Erro ao validar o formulario", request.getRequestURI());
+		for(FieldError x : e.getBindingResult().getFieldErrors()) {
+			erro.addError(x.getField(), x.getDefaultMessage());
+		}
 		return ResponseEntity.status(status).body(erro);
 	}
 }
