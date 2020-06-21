c4eb784d8532cadc83ec4a8ebd9fa263fdbc789e
Lucas Aranha <21315381@anhembimorumbi.edu.br>
2020-06-21 18:05:06 -0300

    Inserindo Pedido

--- src/main/java/com/estudando/cursomc/domain/ItemPedido.java
+++ src/main/java/com/estudando/cursomc/domain/ItemPedido.java
@@ -11,19 +11,17 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @Entity
 public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
-	
+
 	private Double desconto;
 	private Integer quantidade;
 	private Double preco;
-	
-	
+
 	public ItemPedido() {
 	}
-
 
 	public ItemPedido(Pedido pedido, Produto produto, Double desconto, Integer quantidade, Double preco) {
 		super();
@@ -37,56 +35,55 @@ public class ItemPedido implements Serializable {
 	public double getSubTotal() {
 		return (preco - desconto) * quantidade;
 	}
-	
+
 	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
-	
+
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
 
 	public Produto getProduto() {
 		return id.getProduto();
 	}
-	
+
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
+	}
+
 	public ItemPedidoPK getId() {
 		return id;
 	}
-
 
 	public void setId(ItemPedidoPK id) {
 		this.id = id;
 	}
 
-
 	public Double getDesconto() {
 		return desconto;
 	}
-
 
 	public void setDesconto(Double desconto) {
 		this.desconto = desconto;
 	}
 
-
 	public Integer getQuantidade() {
 		return quantidade;
 	}
-
 
 	public void setQuantidade(Integer quantidade) {
 		this.quantidade = quantidade;
 	}
 
-
 	public Double getPreco() {
 		return preco;
 	}
 
-
 	public void setPreco(Double preco) {
 		this.preco = preco;
 	}
-
 
 	@Override
 	public int hashCode() {
@@ -95,7 +92,6 @@ public class ItemPedido implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -113,6 +109,5 @@ public class ItemPedido implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
+
 }
