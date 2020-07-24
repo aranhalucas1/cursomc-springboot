3cb65f918c9f8569e4b7a80aba1fb016fa39f064
Lucas Aranha <21315381@anhembimorumbi.edu.br>
2020-07-24 15:27:09 -0300

    Email HTML

--- src/main/java/com/estudando/cursomc/services/EmailService.java
+++ src/main/java/com/estudando/cursomc/services/EmailService.java
@@ -3,8 +3,12 @@ package com.estudando.cursomc.services;
 import com.estudando.cursomc.domain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 
+import javax.mail.internet.MimeMessage;
+
 public interface EmailService {
 
     void sendOrderConfirmationEmail(Pedido obj);
     void sentEmail(SimpleMailMessage msg);
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
+    void sendHtmlEmail(MimeMessage msg);
 }
